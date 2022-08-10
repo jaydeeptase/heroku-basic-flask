3a3495bafa90c1a82d536c048346fdba4feea18d
jaydeeptase <jaydeeptase@gmail.com>
2022-08-10 10:45:36 -0500

    width image

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def homepage():
 @app.route('/canvas.html')
 def canvaspage():
     return """
-    <img src="/static/images/Portfolio.png" />
+    <img src="/static/images/Portfolio.png" width="90%"/>
     """
 
 @app.route('/pgimfact.html')
